1c07c90c6f33c6f320c57c3f8f732179b526e8da
masontylerjames <mason.tylerjames@gmail.com>
2016-11-11 14:51:49 -0800

    updated training script

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,6 @@
 from miditransform import midiToStateMatrix
 from model import model, n_steps
+import numpy as np
 
 
 def train(midifile):
@@ -9,7 +10,10 @@ def train(midifile):
 
 def _train(statematrix):
     neuralnet = model()
-    train = statematrix[0:n_steps]
-    test = statematrix[1:n_steps + 1]
+    train = [np.flatten(state) for state in statematrix[0:n_steps]]
+    test = [np.flatten(state) for state in statematrix[1:n_steps + 1]]
     neuralnet.fit(train, test)
     return neuralnet
+
+if __name__ == '__main__':
+    model = train('../data/train/mozart/mz_311_1_format0.mid')
