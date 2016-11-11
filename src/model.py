0bee4deea89679c6a24a425b8a4afdec0dd83cff
masontylerjames <mason.tylerjames@gmail.com>
2016-11-11 11:07:31 -0800

    edited output_shape of LSTM layer

--- src/model.py
+++ src/model.py
@@ -8,6 +8,6 @@ def model(dropout=0.5):
     input_shape: a tuple determining the shape of the input layer
     '''
     model = Sequential()
-    model.add(LSTM(output_dim=shape, input_shape=shape, activation='sigmoid',
+    model.add(LSTM(output_shape=shape, input_shape=shape, activation='sigmoid',
                    inner_activation='hard_sigmoid'))
     return model
