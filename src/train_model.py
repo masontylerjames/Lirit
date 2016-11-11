3ac10aef5fb3641aef1ab206e9c44f6face28966
masontylerjames <mason.tylerjames@gmail.com>
2016-11-11 11:41:59 -0800

    made train one step function

--- src/train_model.py
+++ src/train_model.py
@@ -2,6 +2,11 @@ from miditransform import midiToStateMatrix
 from model import model
 
 
+def train_one_step(midifile):
+    statematrix = midiToStateMatrix(midifile)
+    model.fit(statematrix[100], statematrix[101])
+
+
 def train(midifile, sample_length=None, samples=None):
     statematrix = midiToStateMatrix(midifile)
     return _train(statematrix, sample_length, samples)
