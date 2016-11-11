729ecbdfe46c89fa3402c776975422c08ddc92ad
masontylerjames <mason.tylerjames@gmail.com>
2016-11-11 15:10:42 -0800

    helper function

--- src/train_model.py
+++ src/train_model.py
@@ -3,6 +3,14 @@ from model import model, n_steps
 import numpy as np
 
 
+def statematrixtoarray(statematrix):
+    arr = [[entry for note in state for entry in note]
+           for state in statematrix]
+    arr = np.array(arr)
+
+    return arr
+
+
 def train(midifile):
     statematrix = midiToStateMatrix(midifile)
     return _train(statematrix)
@@ -10,10 +18,8 @@ def train(midifile):
 
 def _train(statematrix):
     neuralnet = model()
-    train = np.array([[[entry for note in state for entry in note]
-                       for state in statematrix[0:n_steps]]])
-    test = np.array([[[entry for state in state for entry in state]
-                      for state in statematrix[1: n_steps + 1]]])
+    train = statematrixtoarray(statematrix[0:n_steps])
+    test = statematrixtoarray(statematrix[1: n_steps + 1])
     neuralnet.fit(train, test)
     return neuralnet
 
