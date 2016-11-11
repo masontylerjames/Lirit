f1faca3e62da2bbda7a309238e9eb337f481319d
masontylerjames <mason.tylerjames@gmail.com>
2016-11-11 11:11:38 -0800

    changed train architecture

--- src/train_model.py
+++ src/train_model.py
@@ -2,7 +2,11 @@ from miditransform import midiToStateMatrix
 from model import model
 
 
-def train(midifile):
+def train(midifile, sample_length=None, samples=None):
     statematrix = midiToStateMatrix(midifile)
+    return _train(statematrix, sample_length, samples)
+
+
+def _train(statematrix, sample_length, samples):
     neuralnet = model()
-    pass
+    return neuralnet
