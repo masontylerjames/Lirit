84527cc89628bcb13d53fca890d851e7a8197ca9
masontylerjames <mason.tylerjames@gmail.com>
2016-11-12 19:21:16 -0800

    compose creates a midi file now

--- src/compose.py
+++ src/compose.py
@@ -1,5 +1,6 @@
 from model import input_shape
-from model_train import offset
+from train_model import offset
+from miditransform import noteStateMatrixToMidi
 import numpy as np
 
 
@@ -10,4 +11,13 @@ def compose(model, length, seed=None):
     statematrix = None
     if seed is None:
         seed = np.random.random(input_shape)
-    return model.predict(seed)
+        seed = seed[np.newaxis]
+    predict = model.predict(seed)
+    statematrix = cleanstatematrix(predict)
+    noteStateMatrixToMidi(statematrix)
+
+
+def cleanstatematrix(statematrix):
+    sample = np.random.random(statematrix.shape)
+    sm = sample < statematrix
+    return sm
