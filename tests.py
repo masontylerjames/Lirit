730f629928a72e3ff7b9858d3f8b07ac34de9314
masontylerjames <mason.tylerjames@gmail.com>
2016-11-15 11:06:43 -0800

    added new test func

--- tests.py
+++ tests.py
@@ -15,5 +15,18 @@ def predict(lirit, n=0):
     X, Y = generateXY(sm, 256, 128)
     return lirit.model.predict(np.asarray([X[n]]))
 
+
+def notes_on(lirit, n=0, **kwargs):
+    train_one(lirit)
+    probs = predict(lirit)
+    sm = midiToStateMatrix('data/train/mozart/mz_311_1_format0.mid')
+    X, Y = generateXY(sm, 256, 128)
+    X = np.array(X)
+    Y = np.array(Y)
+    pred = cleanstatematrix(probs)
+    notecount1 = np.array([np.sum(state.T[1]) for state in X[0]])
+    notecount2 = np.array([np.sum(state.T[1]) for state in pred[0]])
+    print notecount1.mean(), notecount2.mean()
+
 if __name__ == '__main__':
     lirit = Lirit()
