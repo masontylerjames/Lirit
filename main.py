3c5db08563bed40be9d76ac6466b715cf8aa9f7b
masontylerjames <mason.tylerjames@gmail.com>
2016-11-16 12:04:27 -0800

    changed main to train a model instead of load one

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == '__main__':
     lirit = Lirit()
-    lirit.load('data/models/newtest.h5')
+    lirit.fitmidis('data/mozart/mz_311_1_format0.mid')
     l = 5497 / 2
     sm = midiToStateMatrix('data/train/mozart/mz_311_1_format0.mid')
     seed = np.array(sm)[:lirit.n_steps][np.newaxis]
