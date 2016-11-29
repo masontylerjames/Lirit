33d1108e77a18074226e1843c550c4a6d34da1b2
masontylerjames <mason.tylerjames@gmail.com>
2016-11-29 13:06:20 -0800

    move featuresmodel, finalize model in Lirit

--- Lirit.py
+++ Lirit.py
@@ -7,7 +7,7 @@ from src.fit import getfiles, generateInputsAndTargets
 from src.features import features_shape, noteStateMatrixToInputForm
 from src.miditransform import noteStateMatrixToMidi, midiToStateMatrix
 import numpy as np
-from featuresmodel import addfeatures
+from src.featuresmodel import addfeatures
 
 
 class Lirit(object):
@@ -108,162 +108,35 @@ def calcConservatism(n, conservatism):
     return conservatism
 
 
-def model_shortdropout(n_steps):
-    # short onoff convolution
-    silos = []
-
-    # adding beat info seemed successful
-    in_shape = (n_steps, features_shape[0], features_shape[1])
-    base_input = Input(shape=in_shape, name='sm_slice')
-
-    # shared weights between on/off and actuation features
-    features_input = [Lambda(lambda x: x[:, :, :, i], output_shape=(n_steps, features_shape[0]))(base_input)
-                      for i in range(2)]
-    lstm_1 = LSTM(256, name='sw_lstm_1')
-    layer_1 = [lstm_1(layer) for layer in features_input]
-    dropout = Dropout(.25)
-    layer_drop_1 = [dropout(layer) for layer in layer_1]
-    dense_2 = Dense(features_shape[0], name='sw_dense_2')
-    layer_4 = [dense_2(layer) for layer in layer_drop_1]
-    reshape_1 = Reshape(
-        (features_shape[0], 1), name='sw_concat_prepare')
-    layer_5 = [reshape_1(layer) for layer in layer_4]
-    stitch_shared = merge(layer_5, mode='concat',
-                          concat_axis=-1, name='sw_out_prepare')
-    silos.append(stitch_shared)
-
-    # accept beat input and neural net that
-    beat_input = Input(shape=(n_steps, 4), name='beat')
-    beat_lstm = LSTM(16, name='beat_lstm')(beat_input)
-    beat_dense = Dense(
-        features_shape[0] * 2, name='beat_dense_2')(beat_lstm)
-    beat_reshape = Reshape(
-        (features_shape[0], 2), name='beat_out_prepare')(beat_dense)
-    silos.append(beat_reshape)
-
-    # add convolution to try and bootstrap an understanding of key
-    # through setting initial weights
-    key_reshape_1 = Reshape(
-        (n_steps, features_shape[0], 1), name='key_add_dim')(features_input[0])
-    key_weights = _genKeyWeights3()
-    key_convolution = TimeDistributed(
-        Convolution1D(4, 12, weights=key_weights, name='key_convolution'), name='key_convolutions')(key_reshape_1)
-    key_reshape_2 = TimeDistributed(
-        Reshape((76 * 4,)), name='key_flatten')(key_convolution)
-    key_dropout_1 = Dropout(.25)(key_reshape_2)
-    key_lstm = LSTM(512, name='key_lstm')(key_dropout_1)
-    key_dropout_2 = Dropout(.25)(key_lstm)
-    key_dense_2 = Dense(
-        features_shape[0] * 2, name='key_dense_2')(key_dropout_2)
-    key_reshape_3 = Reshape(
-        (features_shape[0], 2), name='key_prepare')(key_dense_2)
-    silos.append(key_reshape_3)
-
-    # sum silos and then put through sigmoid activation
-    sum_silos = merge(silos, mode='sum', name='sum_silos')
-    out = Activation('sigmoid', name='constrain_out')(sum_silos)
-
-    inputs = [base_input, beat_input]
-    model = Model(input=inputs, output=out)
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-
-    return model
-
-
-def model_words(n_steps):
-    embed_dim = 16
-    base_input = Input(shape=(n_steps, 87))
-    embedding_td = TimeDistributed(
-        Embedding(261, embed_dim, input_length=87))(base_input)
-    reshape_1 = Reshape((n_steps, 87, embed_dim, 1))(embedding_td)
-    n = 1
-    conv_td_td = TimeDistributed(
-        TimeDistributed(Convolution1D(n, embed_dim)))(reshape_1)
-    flatten_1 = TimeDistributed(Flatten())(conv_td_td)
-    lstm = LSTM(128 * n)(flatten_1)
-    dense = Dense(174)(lstm)
-    reshape_2 = Reshape((87, 2))(dense)
-
-    model = Model(input=base_input, output=reshape_2)
+def model():
+    n_steps = 128
+    n = 173
+    dropout = 0.5
+    input_layer = Input(shape=(n_steps, 87, n))
+    permute_1 = Permute((2, 1, 3))(input_layer)
+    with tf.device('/gpu:0'):
+        per_pitch_lstm_1 = TimeDistributed(
+            LSTM(256, return_sequences=True), name='pitch_lstms_1')(permute_1)
+        dropout_1 = Dropout(dropout)(per_pitch_lstm_1)
+    with tf.device('/gpu:1'):
+        per_pitch_lstm_2 = TimeDistributed(
+            LSTM(256), name='pitch_lstms_2')(dropout_1)
+        dropout_2 = Dropout(dropout)(per_pitch_lstm_2)
+    with tf.device('/gpu:2'):
+        pitch_lstm_1 = Bidirectional(LSTM(128, return_sequences=True))(
+            dropout_2)
+        dropout_3 = Dropout(dropout)(pitch_lstm_1)
+    with tf.device('/gpu:3'):
+        pitch_lstm_2 = Bidirectional(
+            LSTM(64, return_sequences=True))(dropout_3)
+        dropout_4 = Dropout(dropout)(pitch_lstm_2)
+    with tf.device('/gpu:4'):
+        dense_1 = TimeDistributed(Dense(32))(dropout_4)
+        dense_2 = TimeDistributed(Dense(2))(dense_1)
+        out = Activation('sigmoid')(dense_2)
+    model = Model(input=input_layer, output=out)
     model.compile(optimizer='rmsprop', loss='binary_crossentropy')
     return model
-
-
-def model(n_steps):
-    # short onoff convolution
-    silos = []
-
-    # adding beat info seemed successful
-    in_shape = (n_steps, features_shape[0], features_shape[1])
-    base_input = Input(shape=in_shape, name='sm_slice')
-
-    # shared weights between on/off and actuation features
-    features_input = [Lambda(lambda x: x[:, :, :, i], output_shape=(n_steps, features_shape[0]))(base_input)
-                      for i in range(2)]
-    lstm_1 = LSTM(256, return_sequences=True, name='sw_lstm_1')
-    layer_1 = [lstm_1(layer) for layer in features_input]
-    lstm_2 = LSTM(256, name='sw_lstm_2')  # deeper
-    layer_2 = [lstm_2(layer) for layer in layer_1]
-    dense_1 = Dense(256, name='sw_dense_1')  # deeper
-    layer_3 = [dense_1(layer) for layer in layer_2]
-    dense_2 = Dense(features_shape[0], name='sw_dense_2')
-    layer_4 = [dense_2(layer) for layer in layer_3]
-    reshape_1 = Reshape(
-        (features_shape[0], 1), name='sw_concat_prepare')
-    layer_5 = [reshape_1(layer) for layer in layer_4]
-    stitch_shared = merge(layer_5, mode='concat',
-                          concat_axis=-1, name='sw_out_prepare')
-    silos.append(stitch_shared)
-
-    # accept beat input and neural net that
-    beat_input = Input(shape=(n_steps, 4), name='beat')
-    beat_lstm = LSTM(16, name='beat_lstm')(beat_input)
-    beat_dense_1 = Dense(256, name='beat_dense_1')(beat_lstm)
-    beat_dense = Dense(
-        features_shape[0] * 2, name='beat_dense_2')(beat_dense_1)
-    beat_reshape = Reshape(
-        (features_shape[0], 2), name='beat_out_prepare')(beat_dense)
-    silos.append(beat_reshape)
-
-    # add convolution to try and bootstrap an understanding of key
-    # through setting initial weights
-    key_reshape_1 = Reshape(
-        (n_steps, features_shape[0], 1), name='key_add_dim')(features_input[0])
-    key_weights = _genKeyWeights3()
-    key_convolution = TimeDistributed(
-        Convolution1D(4, 12, weights=key_weights, name='key_convolution'), name='key_convolutions')(key_reshape_1)
-    key_reshape_2 = TimeDistributed(
-        Reshape((76 * 4,)), name='key_flatten')(key_convolution)
-    key_lstm = LSTM(128, name='key_lstm')(key_reshape_2)
-    key_keys = Dense(48, name='key_regularization')(key_lstm)
-    key_activation = Activation(
-        'softmax', name='key_softmax')(key_keys)
-    key_dense_1 = Dense(256, name='key_dense_1')(key_activation)
-    key_dense_2 = Dense(
-        features_shape[0] * 2, name='key_dense_2')(key_dense_1)
-    key_reshape_3 = Reshape(
-        (features_shape[0], 2), name='key_prepare')(key_dense_2)
-    silos.append(key_reshape_3)
-
-    # sum silos and then put through sigmoid activation
-    sum_silos = merge(silos, mode='sum', name='sum_silos')
-    out = Activation('sigmoid', name='constrain_out')(sum_silos)
-
-    inputs = [base_input, beat_input]
-    model = Model(input=inputs, output=out)
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-
-    return model
-
-
-def _genKeyWeights3():
-    major = np.array([1, -1, 1, -1, 1, 1, -1, 1, -1, 1, -1, 1])
-    nat_minor = np.array([1, -1, 1, 1, -1, 1, -1, 1, 1, -1, 1, -1])
-    har_minor = np.array([1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1])
-    mel_minor = np.array([1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1])
-    keys = [major, nat_minor, har_minor, mel_minor]
-    keys = [key.reshape(12, 1, 1, 1) for key in keys]
-    return [np.concatenate(keys, axis=3), -1 * np.ones(4)]
 
 if __name__ == '__main__':
     pass
---
+++ src/featuresmodel.py
@@ -0,0 +1,54 @@
+from keras.models import *
+from keras.layers import *
+from keras.callbacks import ModelCheckpoint
+from src.miditransform import midiToStateMatrix, lowerBound, upperBound
+from src.fit import *
+import numpy as np
+import tensorflow as tf
+
+
+def addfeatures(statematrix):
+    return np.array([manufacturestate(state) for state in statematrix])
+
+
+def manufacturestate(state):
+    return [manufacturefeatures(note, i, state) for i, note in enumerate(state)]
+
+
+def manufacturefeatures(note, i, state):
+    pitch_class = [0] * 12
+    pitch = lowerBound + i
+    pitch_class[pitch % 12] = 1
+    pitches = [0] * 12
+    for i, n in enumerate(state[:, 0]):
+        if n == 1:
+            pitches[(i + lowerBound) % 12] += 1
+    key_scores = keyscores(state, np.array(pitches))
+    vicinity = get_vicinity(i, state)
+    return note.tolist() + [pitch] + pitch_class + pitches + key_scores + vicinity
+
+
+def keyscores(state, pitches):
+    major = np.array([1, -1, 1, -1, 1, 1, -1, 1, -1, 1, -1, 1])
+    minor = np.array([1, -1, 1, 1, -1, 1, -1, 1, 1, -1, 1, -1])
+    har_minor = np.array([1, -1, 1, 1, -1, 1, -1, 1, 1, -1, -1, 1])
+    mel_minor = np.array([1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1, -1])
+    major_keys = [pitches.dot(
+        np.append(major[i:], major[:i])) for i in range(12)]
+    minor_keys = [pitches.dot(
+        np.append(minor[i:], minor[:i])) for i in range(12)]
+    har_minor_keys = [pitches.dot(
+        np.append(har_minor[i:], har_minor[:i])) for i in range(12)]
+    mel_minor_keys = [pitches.dot(
+        np.append(mel_minor[i:], mel_minor[:i])) for i in range(12)]
+    return major_keys + minor_keys + har_minor_keys + mel_minor_keys
+
+
+def get_vicinity(i, state):
+    vicinity = []
+    for j in range(-24, 25):
+        try:
+            vicinity += state[i + j, :].tolist()
+        except:
+            vicinity += [0, 0]
+    return vicinity
